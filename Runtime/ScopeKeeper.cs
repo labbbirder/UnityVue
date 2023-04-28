fc2f10fe6a0a7906fad4b24865d7766a5e145b5f
bbbirder <502100554@qq.com>
2023-04-28 17:33:25 +0800

    remove legacy codes

--- Runtime/ScopeKeeper.cs
+++ Runtime/ScopeKeeper.cs
@@ -6,27 +6,18 @@ using System.Linq;
 
 namespace com.bbbirder{
 
-    // public static class Extension{
-    //     public static void AttachToKeeper(this CSReactive.WatchScope scp,ScopeKeeper keeper){
-    //         keeper.onUpdate+=scp.OnUpdate;
-    //     }
-    // }
     [ExecuteInEditMode]
     public class ScopeKeeper : MonoBehaviour
     {
         public static ScopeKeeper Instance;
         public Action onUpdate = CSReactive.WatchScope.UpdateDirtyScopes;
         
-        // Update is called once per frame
         void Awake(){
             Instance = this;
         }
         void LateUpdate()
         {
             onUpdate?.Invoke();
-        }
-        public void KeepMany(params Action[] args){
-            args.ToList().ForEach(a=>CSReactive.WatchEffect(a));
         }
     }
 }
