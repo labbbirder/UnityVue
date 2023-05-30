5cce852240a0a411fe953ef4ca9a0b4127e139af
bbbirder <502100554@qq.com>
2023-05-30 11:42:00 +0800

    fix: Install command

--- Editor/Installer.cs
+++ Editor/Installer.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
-internal class Installer
+internal partial class Installer
 {
+    const string ExportPath = "Assets/Plugins/com.bbbirder.csreactive";
     static string AssetGUID = "f45c27409355d4b40891fc219f340827";
-    [InitializeOnLoadMethod]    
+    [MenuItem("Tools/bbbirder/Install CSReactive")]    
     static void InstallUnityPackage()
     {
-        if(!Directory.Exists("Assets/Plugins/com.bbbirder.csreactive")){
+        if(!Directory.Exists(ExportPath)){
             var assetPath = AssetDatabase.GUIDToAssetPath(AssetGUID);
             AssetDatabase.ImportPackage(assetPath,false);
         }
+    }
+
+}
+
+partial class Installer:AssetPostprocessor{
+    const bool AutoInstall = true;
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) {
+        if(AutoInstall) InstallUnityPackage();
     }
 }
