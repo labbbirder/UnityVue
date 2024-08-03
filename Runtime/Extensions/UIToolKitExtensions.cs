257542cade642add3b4bdc53bce6afcbec5825c6
labbbirder <502100554@qq.com>
2024-08-04 01:12:54 +0800

    feat: utk bind

---
+++ Runtime/Extensions/UIToolKitExtensions.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Numerics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using UnityEngine.Assertions;
+using UnityEngine.UIElements;
+namespace BBBirder.UnityVue
+{
+    using static CastUtils;
+    public static class UIToolKitExtensions
+    {
+
+        public static WatchScope Bind<T>(this INotifyValueChanged<T> control, Expression<Func<object>> getter)
+        {
+            var bodyExp = getter.Body is UnaryExpression castOp ? castOp.Operand : getter.Body;
+            var memberExp = bodyExp as MemberExpression;
+            Assert.IsNotNull(memberExp, $"{bodyExp.NodeType} must be a member getter");
+            var property = memberExp.Member as PropertyInfo;
+            Assert.IsNotNull(memberExp, "member must be a property");
+            var targetGetter = Expression.Lambda(memberExp.Expression).Compile() as Func<object>;
+            var target = targetGetter();
+
+            Func<T, object> memberSetCaster = property.PropertyType.IsAssignableFrom(typeof(T)) ?
+                null : v => DynamicCast(v, property.PropertyType);
+            Func<object, T> ctrlSetCaster = typeof(T).IsAssignableFrom(property.PropertyType) ?
+                null : m => (T)DynamicCast(m, typeof(T));
+            control.RegisterValueChangedCallback(e =>
+            {
+                property.SetValue(target, memberSetCaster != null ? memberSetCaster(e.newValue) : e.newValue);
+            });
+            var scope = CSReactive.WatchEffect(() =>
+            {
+                var memberValue = property.GetValue(target);
+                control.SetValueWithoutNotify(ctrlSetCaster != null ? ctrlSetCaster(memberValue) : (T)memberValue);
+            });
+            return scope;
+        }
+    }
+}
